f4f831c534cb07e9da2fe88bff5ec8ffa863fe97
lktay <leotay0807@gmail.com>
2022-12-09 16:36:32 +0000

    added JS

---
+++ Lesson 1/exercise 1/main.js
@@ -0,0 +1,24 @@
+console.log("age")
+let age = 26;
+console.log(age);
+
+console.log("Number of Rabbits")
+let numberOfRabbits = 2;
+numberOfRabbits = numberOfRabbits * 3;
+console.log(numberOfRabbits)
+
+console.log("bodmas 1")
+let bodmasExample = 3;
+console.log(bodmasExample + 5 / 6);
+
+console.log("bodmas 2")
+bodmasExample = (bodmasExample + 5) / 6
+console.log(bodmasExample);
+
+console.log("random")
+let randomNumber = Math.random() * 5 + 1
+console.log(randomNumber);
+
+console.log("rando 2")
+const randomNum = Math.floor(Math.random() * 6 + 1)
+console.log(randomNum);
